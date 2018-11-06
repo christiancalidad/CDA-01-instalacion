ba6d0c502e978cfbe724e0a0cecfb04989d88c61
jdvelasq <jdvelasq@unal.edu.co>
2018-11-06 15:50:09 -0500

    ubuntu without R

--- ubuntu.sh
+++ ubuntu.sh
@@ -2,7 +2,7 @@
 ## Python
 ## 
 wget https://raw.githubusercontent.com/jdvelasq/CDA-01-instalacion/master/requirements.txt
-wget https://raw.githubusercontent.com/jdvelasq/CDA-01-instalacion/master/requirements-R.txt
+# wget https://raw.githubusercontent.com/jdvelasq/CDA-01-instalacion/master/requirements-R.txt
 wget https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
 bash  Anaconda3-5.3.0-Linux-x86_64.sh
 sudo chmod u=rwx,g=rwx,o=rwx /usr/local/share
@@ -27,25 +27,26 @@ conda install networkx
 ##
 ## R
 ##
-sudo apt install r-base
+# sudo apt install r-base
 
-sudo apt-get install libcurl4-openssl-dev
-sudo apt-get install build-essential
-sudo apt-get install libcurl4-gnutls-dev
-sudo apt-get install libxml2-dev
-sudo apt-get install libssl-dev
+# sudo apt-get install libcurl4-openssl-dev
+# sudo apt-get install build-essential
+# sudo apt-get install libcurl4-gnutls-dev
+# sudo apt-get install libxml2-dev
+# sudo apt-get install libssl-dev
 
-sudo Rscript -e "install.packages(readLines('requirements-R.txt'), repos='https://cloud.r-project.org')"
-sudo Rscript -e "devtools::install_github('IRkernel/IRkernel')"
-sudo Rscript -e "IRkernel::installspec(user = FALSE)"
-sudo Rscript -e "source('http://bioconductor.org/biocLite.R'); biocLite('rhdf5')"
-sudo Rscript -e "install_github('ramnathv/rCharts', 'ramnathv')"
+# sudo Rscript -e "install.packages(readLines('requirements-R.txt'), repos='https://cloud.r-project.org')"
+# sudo Rscript -e "devtools::install_github('IRkernel/IRkernel')"
+# sudo Rscript -e "IRkernel::installspec(user = FALSE)"
+# sudo Rscript -e "source('http://bioconductor.org/biocLite.R'); biocLite('rhdf5')"
+# sudo Rscript -e "install_github('ramnathv/rCharts', 'ramnathv')"
 
 ##
 ## RStudio
 ##
-wget https://download1.rstudio.org/rstudio-1.1.463-amd64.deb
-deb rstudio-1.1.463-amd64.deb
+# wget https://download1.rstudio.org/rstudio-1.1.463-amd64.deb
+# deb rstudio-1.1.463-amd64.deb
+
 source ~/.bash_profile
 
 
